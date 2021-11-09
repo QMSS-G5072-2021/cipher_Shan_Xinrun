8f7721ad11d2f8e901b18432fb0075811c0bdc7a
XinrunS <91167725+XinrunS@users.noreply.github.com>
2021-11-09 11:24:52 -0500

    Update cipher_xs2423.py

--- src/cipher_xs2423/cipher_xs2423.py
+++ src/cipher_xs2423/cipher_xs2423.py
@@ -1,4 +1,26 @@
 def cipher(text, shift, encrypt=True):
+    
+    """
+    This is a package to encipher the input.
+
+    Parameters
+    ----------
+    text: a sequence of string
+    shift: an integer number of shifts
+    encrypt: boolean value True or False
+
+    Returns
+    -------
+    encrypted string
+
+    Examples
+    --------
+    >>> from cipher_xs2423 import cipher_xs2423
+    >>> text = 'abcde'
+    >>> shift = 1
+    >>> cipher_xs2423.cipher(text,shift）
+    ‘bcdef'
+    """
     alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
     new_text = ''
     for c in text:
